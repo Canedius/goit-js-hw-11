452101df13ba742e03cdb9cccfe738be23966b51
Denys <kanedius@gmail.com>
2022-02-17 17:31:08 +0200

    add gallery

---
+++ src/components/servise_api.js
@@ -0,0 +1,43 @@
+import Notiflix from 'notiflix';
+export default class ApiService {
+  constructor() {
+      this.searchQuery = ""   
+      this.page = 1
+      this.pageper_page = 0
+  }
+  async fetchArticles() {
+    const URL = 'https://pixabay.com/api/';
+
+    const options = {
+      params: {
+        key: '25718125-06c0b5156c0d80be6290ce911',
+        q: this.searchQuery,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page : this.page
+      },
+    };
+    try {
+      const axios = require('axios').default
+      return axios.get(URL,options).then(res => {
+        console.log(res.data);
+        Notiflix.Notify.info(`Hooray! We found ${res.data.totalHits - this.pageper_page } images.`)
+        this.pageper_page += 40
+        this.page +=1
+        return res.data.hits}).catch(erorr => Notiflix.Notify.info("We're sorry, but you've reached the end of search results."));
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  resetPage(){
+    this.page = 1
+  }
+  set query (newQuery){
+      this.searchQuery = newQuery
+      
+  }
+}
+
---
+++ src/index.js
@@ -0,0 +1,94 @@
+import './sass/main.scss';
+import SimpleLightbox from 'simplelightbox';
+import Notiflix from 'notiflix';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import NewsApi from './components/servise_api';
+
+
+const loadMoreBtn = document.querySelector('.load-more');
+const galleryEL = document.querySelector(".gallery")
+const formEl = document.querySelector(".search-form")
+const inputEl = document.querySelector(".input")
+const newsApi = new NewsApi()
+
+formEl.addEventListener("submit",loadImages)
+function loadImages(e) {
+ e.preventDefault();
+ clearMarkup()
+ newsApi.page = 1
+ newsApi.query = e.currentTarget.elements.searchQuery.value.trim()
+ loadMore()
+ newsApi.resetPage()
+inputEl.value =""
+ 
+}
+
+
+function createMarcup(params) {
+  if (!params.length) {
+  return Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+  };
+  const lightbox = new SimpleLightbox(".gallery a", { close: true }).refresh()
+  const markup = renderMarkup(params)
+  galleryEL.insertAdjacentHTML('beforeend', markup)
+  
+  const { height: cardHeight } = document
+  .querySelector('.gallery')
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: 'smooth',
+});
+ 
+  
+}
+
+function renderMarkup(params) {
+  console.log(params);
+  return params.map(({webformatURL,largeImageURL,tags,likes,views,comments,downloads })=>{
+    return `
+    <div class="photo-card">
+    <a class ="img-link" href ="${largeImageURL}">
+    <img class = "img-card"  src="${webformatURL}" alt="${tags}" height="240" width="360 loading="lazy" /></a>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b><br>
+        ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b><br>${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b><br>${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b><br>${downloads}
+      </p>
+    </div>
+    </div>`
+    
+  }).join("")
+  
+}
+
+function clearMarkup(params) {
+  galleryEL.innerHTML = ""
+  
+}
+async function loadMore(params) {
+  if (!newsApi.searchQuery) {
+    return
+  }
+  newsApi.fetchArticles().then(createMarcup)
+  
+}
+
+
+const observer = new IntersectionObserver(loadMore, {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1,
+});
+observer.observe(loadMoreBtn);
+
